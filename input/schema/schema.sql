60cace792f84399f10c122fb75dbe46536fe129b
Hokie <52640930+rebeca-lamas@users.noreply.github.com>
2019-10-14 21:27:48 -0400

    small format changes

--- input/schema/schema.sql
+++ input/schema/schema.sql
@@ -1,34 +1,33 @@
-CREATE DATABASE ...
-CREATE TABLE ...
-create table elementary_schools (
+CREATE TABLE elementary_schools (
 	school_name VARCHAR NOT NULL,
 	school_rating INT NOT NULL,
 	grade_levels INT NOT NUll,
 	percent_low_income INT NOT NULL,
-	foreign key (zip_code) refernces median_sales_price (zip_code),
-	primary key (zip_code)
+	FOREIGN KEY (zip_code) REFERENCES median_sales_price (zip_code),
+	PRIMARY KEY (zip_code)
 );
 
-Create Table middle_schools (
+CREATE TABLE middle_schools (
 	school_name VARCHAR NOT NULL,
 	school_rating INT NOT NULL,
 	grade_level INT NOT NULL,
 	percent_low_income INT NOT NUll,
-	foreign key (zip_code) refernces median_sales_price (zip_code),
-	primary key (zip_code)
+	FOREIGN KEY (zip_code) REFERENCES median_sales_price (zip_code),
+	PRIMARY KEY (zip_code)
 );
 
-Create Table high_school (
-	school Varchar NOT NUll,
-	school_rating INt NOT NULL,
+CREATE TABLE high_school (
+	school VARCHAR NOT NUll,
+	school_rating INT NOT NULL,
 	grade_level INT NOT NULL,
 	percent_low_income INT NOT NULL,
-	foreign key (zip_code) refernces median_sales_price (zip_code),
-	primary key (zip_code)
+	FOREIGN KEY (zip_code) REFERENCES median_sales_price (zip_code),
+	PRIMARY KEY (zip_code)
 );
 
-Create Table median_sales_price (
+CREATE TABLE median_sales_price (
 	zip_code INT NOT NULL,
 	state_name VARCHAR NOT NULL,
 	2018_median_sales_price INT NOT NULL
+	PRIMARY KEY (zip_code)
 );
